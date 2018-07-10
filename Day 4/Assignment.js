b6c8a4ff55c050b6fcd0db76372f52fe5e007314
Xpo0444 <40860135+sbharadwajingenuity@users.noreply.github.com>
2018-07-10 18:17:51 +0530

    Comments added

--- Day 4/Assignment.js
+++ Day 4/Assignment.js
@@ -1,14 +1,15 @@
+// function to show progress
 function progress(){
     let element = document.getElementById("progressBar");
-    let width = 5;
+    let width = 5; //starting percentage
     let id = setInterval(function (){
         if(width >= 100)
         {
-            clearInterval(id);
+            clearInterval(id);  //when reaches 100, halt
         }
         else
         {
-            width = width+1;
+            width = width+1;    //increase percentage by 1 everytime.
             element.style.width = width + "%";
             element.innerHTML = width * 1 + "%";
         }
