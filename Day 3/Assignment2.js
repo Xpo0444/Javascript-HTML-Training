24b760437b3a498e5f8a9fff7732c717c2ec927f
sbharadwajingenuity <40860135+sbharadwajingenuity@users.noreply.github.com>
2018-07-10 15:02:00 +0530

    Assignment2

---
+++ Day 3/Assignment2.js
@@ -0,0 +1,27 @@
+// initialising object 1
+let x = {
+name : "Shan",
+age : 25
+}
+// initialising object 1
+let y = {
+name : "Subu",
+age : 25
+}
+
+//defining function to check
+let show;
+if('name','cold' in x && 'name','cold' in y)
+{
+show = function(){
+    alert("yes");
+};
+}
+else
+{
+show = function(){
+alert("No");
+};
+}
+//calling function
+show();
